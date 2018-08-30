eb89020cab1a7096f4044b7994036a7296b4a56c
Danilo Fernandes <danilofernandes45@gmail.com>
2018-08-30 19:40:37 -0300

    cv_filter rotine

--- Code/polsar_analysis_29_08_18.r
+++ Code/polsar_analysis_29_08_18.r
@@ -1,6 +1,37 @@
 library(raster)
 library(png)
 
+
+cv_filter <- function(hhhh, hvhv, vvvv, window_dim){
+  
+  parallel_cv <- function(x) localFun(x, x, ngb = window_dim, fun = function(x,y) sd(x)/mean(x))
+  
+  beginCluster()
+  
+  hhhh_cv <- clusterR(hhhh, parallel_cv, filename = "hhhh_cv.grd")
+  hvhv_cv <- clusterR(hvhv, parallel_cv, filename = "hvhv_cv.grd")
+  vvvv_cv <- clusterR(vvvv, parallel_cv, filename = "vvvv_cv.grd")
+  
+  endCluster()
+  
+  #dimensões dos blocos dos objetos RasterLayer filtrados do qual deve-se gerar a imagem
+  begin_row <- 1
+  begin_col <- 1
+  nrows <- nrow(hhhh_cv)
+  ncols <- ncol(hhhh_cv)
+  
+  matrix_cv <- array(0, dim = c(nrows, ncols, 3))
+  
+  matrix_cv[,,1] <- getValuesBlock(hhhh_cv, nrow = begin_row, nrows = nrows, ncol = begin_col, ncols = ncols, format = 'matrix')
+  matrix_cv[,,2] <- getValuesBlock(hvhv_cv, nrow = begin_row, nrows = nrows, ncol = begin_col, ncols = ncols, format = 'matrix')
+  matrix_cv[,,3] <- getValuesBlock(vvvv_cv, nrow = begin_row, nrows = nrows, ncol = begin_col, ncols = ncols, format = 'matrix')
+  
+  writePNG(matrix_cv, target = "image.png")
+  
+  
+}
+
+
 # 1. Criação de cabeçalho
 
 # 1.1. Dimensões da imagem
@@ -18,23 +49,4 @@ hhhh <- raster("hhhh.grd")
 hvhv <- raster("hvhv.grd")
 vvvv <- raster("vvvv.grd")
 
-parallel_cv <- function(x) localFun(x, x, ngb = 3, fun = function(x,y) sd(x)/mean(x))
-
-beginCluster()
-
-hhhh_cv <- clusterR(hhhh, parallel_cv, filename = "hhhh_cv.grd")
-hvhv_cv <- clusterR(hvhv, parallel_cv, filename = "hvhv_cv.grd")
-vvvv_cv <- clusterR(vvvv, parallel_cv, filename = "vvvv_cv.grd")
-
-endCluster()
-
-nrow <- nrow(hhhh_cv)
-ncol <- ncol(hhhh_cv)
-
-matrix_cv <- array(0, dim = c(nrow, ncol, 3))
-
-matrix_cv[,,1] <- getValuesBlock(hhhh_cv, nrow = 1, nrows = nrow, ncol = 1, ncols = ncol, format = 'matrix')
-matrix_cv[,,2] <- getValuesBlock(hvhv_cv, nrow = 1, nrows = nrow, ncol = 1, ncols = ncol, format = 'matrix')
-matrix_cv[,,3] <- getValuesBlock(vvvv_cv, nrow = 1, nrows = nrow, ncol = 1, ncols = ncol, format = 'matrix')
-
-writePNG(matrix_cv, target = "image.png")
+cv_filter(hhhh, hvhv, vvvv, window_dim = 3)
