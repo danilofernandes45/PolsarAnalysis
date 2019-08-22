665fa379e0400f342a72d047eabf97d6d7884053
danilofernandes45 <danilofernandes45@gmail.com>
2019-08-21 21:05:51 -0300

    Analysis

--- Code/read_data_06_12_18.R
+++ Code/read_data_06_12_18.R
@@ -129,7 +129,7 @@ writePNG(matrix, target = "test.png")
 #Regions selected
 writePNG(matrix[100:155, 1:80,], target = "region1_soybeans_232_231.png")
 
-writePNG(matrix[100:170, 55:80,], target = "region1_soybeans_232.png")
+writePNG(matrix[100:165, 40:80,], target = "region1_soybeans_232.png")
 
 writePNG(matrix[25:80, 30:80,], target = "region2_canola_43.png")
 
--- Data/Subset_SMAPVEX16_FQ15W_ACF/Analysis_Regions.R
+++ Data/Subset_SMAPVEX16_FQ15W_ACF/Analysis_Regions.R
@@ -1,17 +1,19 @@
 plotScattererMap(dim)
 plotHistogramBeta("trihedral", dim, filter = TRUE)
 data <- getFilteredData("trihedral", dim)
+mean(data)
 ksTestBeta("trihedral", dim, filter = TRUE)
 
 plotHistogramBeta("random volume", dim, filter = TRUE)
 data <- getFilteredData("random volume", dim)
+mean(data)
 ksTestBeta("random volume", dim, filter = TRUE)
 
 #===========================================================
 
 tri_alpha <- data.frame(
   X = 1:5,
-  SB231_232 = c(3.149, 4.32, 4.401, 4.573, 4.493), #SOYBEANS 231-232
+  SB232 = c(3.039, 4.752, 4.553, 4.217, 4.02), #SOYBEANS 232
   CN43 = c(2.65, 3.546, 5.529, 5.567, 6.951), #CANOLA 43
   WT255 = c(2.743, 4.385, 5.802, 5.442, 4.813) #WHEAT 255
   
@@ -19,28 +21,34 @@ tri_alpha <- data.frame(
 
 tri_beta <- data.frame(
   X = 1:5,
-  SB231_232 = c(11.789, 14.22, 13.332, 13.698, 12.304), #SOILBEANS 231-232
+  SB232 = c(11.602, 15.506, 13.68, 12.532, 11.28), #SOYBEANS 232
   CN43 = c(10.603, 11.911, 14.862, 14.06, 16.534), #CANOLA 43
   WT255 = c(10.169, 13.309, 17.049, 15.24, 13.007) #WHEAT 255
   
 )
 
+tri_mean <- tri_alpha / (tri_alpha + tri_beta)
+tri_mean$X <- 1:5
+
 rv_alpha <- data.frame(
   X = 1:5,
-  SB231_232 = c(8.343, 7.853, 5.892, 6.19, 5.999), #SOILBEANS 231-232
+  SB232 = c(9.334, 8.654, 5.902, 5.891, 6.582), #SOYBEANS 231-232
   CN43 = c(6.954, 5.886, 5.56, 6.682, 6.028), #CANOLA 43
   WT255 = c(5.784, 6.442, 6.552, 6.976, 6.137) #WHEAT 255
   
 )
 
 
-rv_beta <- c(
+rv_beta <- data.frame(
   X = 1:5,
-  SB231_232 = c(17.102, 15.553, 10.829, 11.269, 10.28), #SOILBEANS 231-232
+  SB232 = c(18.516, 17.722, 10.969, 10.621, 11.241), #SOYBEANS 231-232
   CN43 = c(12.92, 11.54, 9.648, 10.779, 10.104), #CANOLA 43
   WT255 = c(10.578, 12.226, 12.212, 12.719, 11.37) #WHEAT 255
   
 )
+
+rv_mean <- rv_alpha / (rv_alpha + rv_beta)
+rv_mean$X <- 1:5
 
 
 library(ggplot2)
@@ -50,5 +58,21 @@ ggplot(aes(x = X, y = value, group = variable, color = variable), data = melt(tr
   geom_point() + geom_line()
 
 ggplot(aes(x = X, y = value, group = variable, color = variable), data = melt(tri_beta, id.vars = "X")) +
+  xlab("Sample") + ylab("Parameter value") +
+  geom_point() + geom_line()
+
+ggplot(aes(x = X, y = value, group = variable, color = variable), data = melt(tri_mean, id.vars = "X")) +
+  xlab("Sample") + ylab("Parameter value") +
+  geom_point() + geom_line()
+
+ggplot(aes(x = X, y = value, group = variable, color = variable), data = melt(rv_alpha, id.vars = "X")) +
+  xlab("Sample") + ylab("Parameter value") +
+  geom_point() + geom_line()
+
+ggplot(aes(x = X, y = value, group = variable, color = variable), data = melt(rv_beta, id.vars = "X")) +
+  xlab("Sample") + ylab("Parameter value") +
+  geom_point() + geom_line()
+
+ggplot(aes(x = X, y = value, group = variable, color = variable), data = melt(rv_mean, id.vars = "X")) +
   xlab("Sample") + ylab("Parameter value") +
   geom_point() + geom_line()
