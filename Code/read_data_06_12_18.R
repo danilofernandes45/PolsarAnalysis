d315d69d66586c6c3a874c643edb01093b8032a2
danilofernandes45 <danilofernandes45@gmail.com>
2019-09-05 14:30:49 -0300

    Analysis

--- Code/read_data_06_12_18.R
+++ Code/read_data_06_12_18.R
@@ -1,3 +1,18 @@
+select_sample <- function(matrix, dim){
+  a <- dim[1]
+  b <- a + dim[2]
+  c <- dim[3]
+  d <- c + dim[4]
+  
+  matrix[(a-2):a,(c-2):(d+2),] <- 1
+  matrix[b:(b+2),(c-2):(d+2),] <- 1
+  
+  matrix[(a-2):(b+2), (c-2):c, ] <- 1
+  matrix[(a-2):(b+2), d:(d+2), ] <- 1
+  
+  return(matrix)
+}
+
 read_int_complex_file <- function(file, nrow, ncol){
   
   complex_numbers <- matrix(
@@ -123,23 +138,46 @@ matrix[,,1] <- ecdf(hhhh)(hhhh)
 matrix[,,2] <- ecdf(hvhv)(hvhv)
 matrix[,,3] <- ecdf(vvvv)(vvvv)
 
-library("png")
-writePNG(matrix, target = "test.png")
+# library("png")
+# writePNG(matrix, target = "test.png")
+# 
+# #Regions selected
+# writePNG(matrix[100:155, 1:80,], target = "region1_soybeans_232_231.png")
+# 
+# writePNG(matrix[100:165, 50:80,], target = "region1_soybeans_232.png")
+# writePNG(matrix[90:155, 1:30,], target = "region2_soybeans_231.png")
+# writePNG(matrix[115:180, 360:390,], target = "region3_soybeans_101.png")
+# 
+# writePNG(matrix[25:80, 30:80,], target = "region2_canola_43.png")
+# writePNG(matrix[180:241, 290:335,], target = "region1_canola224.png")
+# 
+# writePNG(matrix[160:225, 245:280,], target = "region3_wheat_255.png")
+# writePNG(matrix[425:490, 315:350,], target = "region2_wheat_105.png")
+# writePNG(matrix[500:565, 310:345,], target = "region1_wheat_104.png")
+# 
+# writePNG(matrix[415:480, 260:295,], target = "region2_oats_102.png")
+# writePNG(matrix[490:555, 260:295,], target = "region1_oats_103.png")
 
-#Regions selected
-writePNG(matrix[100:155, 1:80,], target = "region1_soybeans_232_231.png")
+#===================================================================
 
-writePNG(matrix[100:165, 50:80,], target = "region1_soybeans_232.png")
-writePNG(matrix[90:155, 1:30,], target = "region2_soybeans_231.png")
-writePNG(matrix[115:180, 360:390,], target = "region3_soybeans_101.png")
+#Pauli Decomposition
+matrix[,,1] <- ecdf(hhhh+vvvv)(hhhh+vvvv)
+matrix[,,2] <- ecdf(abs(hhhh-vvvv))(abs(hhhh-vvvv))
+matrix[,,3] <- ecdf(2*hvhv)(2*hvhv)
 
-writePNG(matrix[25:80, 30:80,], target = "region2_canola_43.png")
-writePNG(matrix[180:241, 290:335,], target = "region1_canola224.png")
+matrix <- select_sample(matrix, c(115, 65, 360, 30)) #SB 101
+matrix <- select_sample(matrix, c(90, 65, 6, 30))    #SB 231
+matrix <- select_sample(matrix, c(100, 65, 50, 30))  #SB 232
 
-writePNG(matrix[160:225, 245:280,], target = "region3_wheat_255.png")
-writePNG(matrix[425:490, 315:350,], target = "region2_wheat_105.png")
-writePNG(matrix[500:565, 310:345,], target = "region1_wheat_104.png")
+matrix <- select_sample(matrix, c(160, 65, 245, 35)) #WT 255
+matrix <- select_sample(matrix, c(444, 65, 370, 35)) #WT 105 ***
+matrix <- select_sample(matrix, c(515, 65, 360, 35)) #WT 104 ***
 
-writePNG(matrix[415:480, 260:295,], target = "region2_oats_102.png")
-writePNG(matrix[490:555, 260:295,], target = "region1_oats_103.png")
+matrix <- select_sample(matrix, c(180, 61, 290, 45)) #CN 224
+matrix <- select_sample(matrix, c(25, 55, 30, 50))   #CN 43
 
+matrix <- select_sample(matrix, c(425, 65, 315, 35)) #OT 102
+matrix <- select_sample(matrix, c(500, 65, 310, 35)) #OT 103
+
+library(png)
+writePNG(matrix, target = "regions_2.png")
