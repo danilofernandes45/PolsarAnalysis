7c77500c3e13f8842b1f8ee7d7c71e17b5c77409
acfrery <acfrery@gmail.com>
2020-03-21 10:42:04 -0300

    Commit before cleaning the LaTeX file

--- Code/PurityPlot.R
+++ Code/PurityPlot.R
@@ -1,5 +1,7 @@
 ### This script collects all the data in a single data frame
 
+require(gridExtra)
+
 ### Purity - Danilo 18/03/20
 
 PurityCanola$Crop <- "Canola"
@@ -12,17 +14,17 @@ Purity$Crop <- as.factor(Purity$Crop)
 
 PurityPlot <- ggplot(Purity, aes(x=Purity, fill=Date)) +
   geom_density(alpha=.5) +
-  labs(x="Purity 2016", y="Estimated Density") +
+  labs(x="Purity 2016", y="Purity Estimated Density") +
   theme_ipsum(base_family = "Times New Roman", 
               base_size = 10, axis_title_size = 10) +
   scale_fill_ipsum() +
-  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"))
-  
-PurityPlot + 
-  theme(legend.position="bottom",
+  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"),
+        legend.position="none", #"bottom" for individual plots
         legend.title=element_blank(),
         legend.margin = margin(rep(0,4), "cm")) + 
   facet_grid(.~ Crop)  
+PurityPlot
+
 ggsave(file="../../../../Figures/GRSL_2020/FactorPlots/Purity.pdf", 
        width = 210, height=60, units="mm", device = cairo_pdf)
 
@@ -38,17 +40,21 @@ Alpha$Crop <- as.factor(Alpha$Crop)
 
 AlphaPlot <- ggplot(Alpha, aes(x=Alpha, fill=Date)) +
   geom_density(alpha=.5) +
-  labs(x="Alpha 2016", y="Estimated Density") +
+  labs(x="Alpha 2016", y="Alpha Estimated Density") +
   theme_ipsum(base_family = "Times New Roman", 
               base_size = 10, axis_title_size = 10) +
   scale_fill_ipsum() +
-  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"))
-
-AlphaPlot + 
-  theme(legend.position="bottom",
+  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"),
+        legend.position="none", #"bottom" for individual plots
         legend.title=element_blank(),
         legend.margin = margin(rep(0,4), "cm")) + 
-  facet_grid(.~ Crop)  
+  facet_grid(.~ Crop) +
+  theme(
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
+  )
+AlphaPlot
+
 ggsave(file="../../../../Figures/GRSL_2020/FactorPlots/Alpha.pdf", 
        width = 210, height=60, units="mm", device = cairo_pdf)
 
@@ -64,22 +70,26 @@ Helicity$Crop <- as.factor(Helicity$Crop)
 
 HelicityPlot <- ggplot(Helicity, aes(x=Helicity, fill=Date)) +
   geom_density(alpha=.5) +
-  labs(x="Helicity 2016", y="Estimated Density") +
+  labs(x="Helicity 2016", y="Helicity Estimated Density") +
   theme_ipsum(base_family = "Times New Roman", 
               base_size = 10, axis_title_size = 10) +
   scale_fill_ipsum() +
-  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"))
-
-HelicityPlot + 
-  theme(legend.position="bottom",
+  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"),
+        legend.position="bottom",
         legend.title=element_blank(),
         legend.margin = margin(rep(0,4), "cm")) + 
-  facet_grid(.~ Crop)  
+  facet_grid(.~ Crop) +
+  theme(
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
+  ) 
+HelicityPlot
+
 ggsave(file="../../../../Figures/GRSL_2020/FactorPlots/Helicity.pdf", 
        width = 210, height=60, units="mm", device = cairo_pdf)
 
 
-#### Only one plot
+#### Only one plot with facet
 
 Purity$Index <- "Purity"
 names(Purity) <- c("Observation", "Date", "Crop", "Index")
@@ -92,3 +102,26 @@ GeodesicIndexes <- rbind(Purity, Alpha, Helicity)
 GeodesicIndexes$Index <- as.factor(GeodesicIndexes$Index)
 
 save(file="/Users/acfrery/Documents/Alunos/Danilo Fernandes/Data/ACFrery/GeodesicIndexes.Rdata", GeodesicIndexes)
+
+ggplot(GeodesicIndexes, aes(x=Observation, fill=Date)) +
+  geom_boxplot(alpha=.5) +
+#  labs(x="Helicity 2016", y="Estimated Density") +
+  theme_ipsum(base_family = "Times New Roman", 
+              base_size = 10, axis_title_size = 10) +
+  scale_fill_ipsum() +
+  theme(plot.margin=grid::unit(c(0,0,0,0), "mm")) + 
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    legend.margin = margin(rep(0, 4), "cm")
+  ) +
+  facet_grid(Index ~ Crop)
+ggsave(file="../../../../Figures/GRSL_2020/FactorPlots/BoxPlotIndexes.pdf", 
+       width = 210, height=200, units="mm", device = cairo_pdf)
+
+#### Only one plot with grid.arrange
+
+grid.arrange(PurityPlot, AlphaPlot, HelicityPlot, nrow=3)
+IndexesPlot <- arrangeGrob(PurityPlot, AlphaPlot, HelicityPlot, nrow=3)
+ggsave(file="../../../../Figures/GRSL_2020/FactorPlots/Indexes.pdf", 
+       width = 210, height=200, units="mm", device = cairo_pdf, IndexesPlot)
